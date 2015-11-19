3bbcd21751e60d796c2cccc6295264b88262a164
Scott Bellware <sbellware@gmail.com>
2015-11-19 14:51:33 -0600

    Stackframe is in its own file

--- lib/error_data.rb
+++ lib/error_data.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'schema'
 require 'casing'
 
+require 'error_data/error_data/stack_frame'
 require 'error_data/error_data'
 require 'error_data/serialize'
 require 'error_data/import'
--- lib/error_data/error_data.rb
+++ lib/error_data/error_data.rb
@@ -1,31 +1,6 @@
 class ErrorData
   include Schema::DataStructure
 
-  class StackFrame
-    include Schema::DataStructure
-
-    attribute :filename, String
-    attribute :line_number, Integer
-    attribute :method_name, String
-
-    def to_s
-      "#{filename}:#{line_number}:in `#{method_name}'"
-    end
-
-    def correspond?(backtrace_line)
-      to_s == backtrace_line
-    end
-
-    def self.parse(line)
-      instance = build
-
-      instance.filename, instance.line_number, instance.method_name = line.split(":")
-      instance.method_name = instance.method_name ? instance.method_name.gsub(/^in `(.*?)'$/, "\\1") : "(none)"
-
-      instance
-    end
-  end
-
   attribute :class_name, String
   attribute :message, String
   attribute :stack_trace, Array[StackFrame]
